419d4ab6b2a53e44c2ce09100bdbc9ab4dfcceb3
bharatpathak1998 <bharatpathak099@gmail.com>
2022-08-25 01:52:57 +0530

    Added : To Find The Maximum String

--- Generics.java
+++ Generics.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 
 public class Generics {
-    static void maxFloat(Float a, Float b, Float c) {
-        Float max = a;
+    static void maxString(String a, String b, String c) {
+        String max = a;
         if (b.compareTo(max) > 0 && b.compareTo(c) > 0) {
             max = b;
         } else if (c.compareTo(max) > 0) {
             max = c;
         }
-        System.out.println("Maximum Number : "+max);
+        System.out.println("Maximum String : "+max);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter The First Number : ");
-        Float a = sc.nextFloat();
-        System.out.println("Enter The Second Number : ");
-        Float b = sc.nextFloat();
-        System.out.println("Enter The Third Number : ");
-        Float c = sc.nextFloat();
+        System.out.println("Enter The First Name : ");
+        String  a = sc.nextLine();
+        System.out.println("Enter The Second Name : ");
+        String  b = sc.nextLine();
+        System.out.println("Enter The Third Name : ");
+        String  c = sc.nextLine();
 
-        maxFloat(a, b, c);
+        maxString(a, b, c);
     }
 }
