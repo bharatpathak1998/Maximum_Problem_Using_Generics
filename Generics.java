66dfc17c80f2b9f06215d2c2294bf99159cfba9d
bharatpathak1998 <bharatpathak099@gmail.com>
2022-08-25 01:55:39 +0530

    Added : Refactor All The Three To One Generic Method

--- Generics.java
+++ Generics.java
@@ -1,25 +1,49 @@
 import java.util.Scanner;
 
 public class Generics {
-    static void maxString(String a, String b, String c) {
-        String max = a;
+    public static <T extends Comparable <T>> T maxTest(T a, T b, T c) {
+        T max = a;
         if (b.compareTo(max) > 0 && b.compareTo(c) > 0) {
             max = b;
         } else if (c.compareTo(max) > 0) {
             max = c;
         }
-        System.out.println("Maximum String : "+max);
+        return max;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter The First Name : ");
-        String  a = sc.nextLine();
-        System.out.println("Enter The Second Name : ");
-        String  b = sc.nextLine();
-        System.out.println("Enter The Third Name : ");
-        String  c = sc.nextLine();
 
-        maxString(a, b, c);
+        System.out.print("Enter First Integer : ");
+        Integer i1 = sc.nextInt();
+        System.out.print("Enter Second Integer : ");
+        Integer i2 = sc.nextInt();
+        System.out.print("Enter Third Integer : ");
+        Integer i3 = sc.nextInt();
+
+        Integer maxInteger = maxTest(i1, i2, i3);
+
+        System.out.print("\nEnter First Float : ");
+        Float f1 = sc.nextFloat();
+        System.out.print("Enter Second Float : ");
+        Float f2 = sc.nextFloat();
+        System.out.print("Enter Third Float : ");
+        Float f3 = sc.nextFloat();
+
+        Float maxFloat = maxTest(f1, f2, f3);
+
+        System.out.print("\nEnter First String : ");
+        String  s1 = sc.nextLine();
+        sc.nextLine();
+        System.out.print("Enter Second String : ");
+        String  s2 = sc.nextLine();
+        System.out.print("Enter Third String : ");
+        String  s3 = sc.nextLine();
+
+        String maxString = maxTest(s1, s2, s3);
+
+        System.out.println("\nMax Integer : "+maxInteger);
+        System.out.println("Max Float : "+maxFloat);
+        System.out.println("Max String : "+maxString);
     }
 }
